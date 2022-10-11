ce43a7ca82a7ba53a31a5d7e06fc5c53e061f22f
Daniel Kiss <83577578+danielkissprezi@users.noreply.github.com>
2022-10-11 14:30:47 +0200

    Long task flag

--- main.cpp
+++ main.cpp
@@ -4,34 +4,40 @@
 
 using namespace std::literals::chrono_literals;
 
+#define LONG_TASKS
+
 int main(int argc, char* argv[]) {
 	Scheduler s;
 
 	for (int j = 0; j < 10'000; j++) {
-		puts("tick");
+		printf("tick %d\n", j);
 		// bunch of app tasks
-		for (int i = 0; i < 100; i++) {
-			s.Dispatch(std::make_unique<Job>(Category::kApp, Priority::kDefault, [] {
-				puts("app");
-				//*
-				std::this_thread::sleep_for(100ms);
-				//*/
+		for (int i = 0; i < 10; i++) {
+			s.Dispatch(std::make_unique<Job>(Category::kApp, Priority::kDefault, [j, i] {
+				printf("app %d %d\n", j, i);
+#ifdef LONG_TASKS
+				std::this_thread::sleep_for(10ms);
+#endif
 			}));
 		}
 		// render job generator job
-		s.Dispatch(std::make_unique<Job>(Category::kRender, Priority::kDefault, [&s] {
-			puts("render dispatch");
+		s.Dispatch(std::make_unique<Job>(Category::kRender, Priority::kDefault, [&s, j] {
+			printf("render dispatch %d\n", j);
 			for (int i = 0; i < 10; i++) {
-				s.Dispatch(std::make_unique<Job>(Category::kRender, Priority::kLow, [] {
-					puts("low prio render");
-					//*
+				s.Dispatch(
+					std::make_unique<Job>(Category::kBackground, Priority::kLow, [i, j] { printf("low prio render %d %d\n", j, i); }));
+			}
+			for (int i = 0; i < 10; i++) {
+				s.Dispatch(std::make_unique<Job>(Category::kBackground, Priority::kDefault, [i, j] {
+					printf("background %d %d\n", j, i);
+#ifdef LONG_TASKS
 					std::this_thread::sleep_for(100ms);
-					//*/
+#endif
 				}));
 			}
 		}));
 		// fast lane is submitted last
-		s.Dispatch(std::make_unique<Job>(Category::kFastLane, Priority::kDefault, [] { puts("fast"); }));
+		s.Dispatch(std::make_unique<Job>(Category::kFastLane, Priority::kDefault, [j] { printf("fast %d\n", j); }));
 		// simulate a little work
 		std::this_thread::sleep_for(10ms);
 	}
