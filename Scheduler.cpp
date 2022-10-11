15aaac8edabf07509e20eeb703d76e65aae6f4b8
Daniel Kiss <83577578+danielkissprezi@users.noreply.github.com>
2022-10-11 14:30:35 +0200

    More dedicated threads

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -1,13 +1,21 @@
 #include "api.h"
 
 Scheduler::Scheduler() : queue(std::make_unique<JobsQueue>()) {
-    const auto n = std::thread::hardware_concurrency();
+	const auto n = std::thread::hardware_concurrency();
 	workers.reserve(n);
 
+	// dedicated workers
 	workers.push_back(std::make_unique<Worker>(CategoryMask(Category::kFastLane), queue.get(), &cv, &cv_m_));
-	for (int i = std::max(int(n) - 1, 1); i >= 0; i--) {
-        workers.push_back(std::make_unique<Worker>(kAllCategories, queue.get(), &cv, &cv_m_));
+	workers.push_back(std::make_unique<Worker>(CategoryMask(Category::kBackground), queue.get(), &cv, &cv_m_));
+	workers.push_back(std::make_unique<Worker>(CategoryMask(Category::kRender), queue.get(), &cv, &cv_m_));
+	workers.push_back(std::make_unique<Worker>(CategoryMask(Category::kApp), queue.get(), &cv, &cv_m_));
+
+	// shared workers increase throughput, if there's available hardware
+	for (int i = int(n) - int(workers.size()); i > 0; i--) {
+		workers.push_back(std::make_unique<Worker>(kAllCategories, queue.get(), &cv, &cv_m_));
 	}
+
+	printf("initialized %lu workers, target n=%d\n", workers.size(), n);
 	jobs.reserve(1024);
 }
 
